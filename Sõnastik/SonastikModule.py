1304da36712ad5d4a4d9714b9d719c51d0938976
markj <markj@MARK>
2025-04-10 10:56:40 +0300

    Add project files.

---
+++ Sõnastik/SonastikModule.py
@@ -0,0 +1,56 @@
+﻿import random
+from statistics import correlation
+
+sonastik = {"koer": "собака", "kass": "кот", "lind": "птица", "jänes": "кролик", "karu": "медведь",}
+
+def tolgi_est_rus(sona):
+    """
+    Tolgib sona eesti keeles vene keelde.
+    """
+    return sonastik.get(sona, "Sona ei ole leidnud")
+
+def tolgi_rus_est(sona):
+    """
+    Tolgib sona vene keeles eesti keelde.
+    """
+    for est, rus in sonastik.items():
+        if rus == sona:
+            return est
+    return "Sona ei ole leidnud"
+
+def lisa_sona(est, rus):
+    """
+    Lisab uue sona sonastikku.
+    """
+    if est in sonastik:
+        return "Sona juba on sonastikus"
+    sonastik[est] = rus
+    return "Sona on lisatud"
+
+def paranda_sona(old_est, new_est, new_rus):
+    """
+    Parandab sona sonastikus.
+    """
+    if old_est not in sonastik:
+        return "Sona ei ole leidnud"
+    sonastik.pop(old_est)
+    sonastik[new_est] = new_rus
+    return "Sona on parandatud"
+
+def testi_teadmisi():
+    if not sonastik:
+        return "Sonastik on tuhi"
+    questions = list(sonastik.items())
+    random.shuffle(questions)
+    correct = 0
+    total = len(questions)
+
+    for est, rus in questions:
+        answer = input(f"Kuidas tolgib '{est}' vene keelte? ")
+        if answer.lower() == rus.lower():
+            print("Oige!")
+            correct += 1
+        else:
+            print(f"Vale. Oige vastus on: {rus}")
+
+    print(f"Test on lopetatud! Teie result on: {round(correct / total * 100)}%")
---
+++ Sõnastik/Sõnastik.py
@@ -0,0 +1,42 @@
+﻿from SonastikModule import *
+
+print ("Tere tulemast sõnastikku!")
+
+while True:
+    print("\nVali:")
+    print("1 - Tolgib eesti to vene")
+    print("2 - Tolgib vene to eesti")
+    print("3 - Lisada uue sona")
+    print("4 - Parandada sona")
+    print("5 - Kontrollida teadet")
+    print("6 - Valja")    
+
+    choice = input("Teie valik: ")
+
+    if choice == "1":
+        word = input("Kirjutage sona eesti keeles").strip()
+        print("Tolg vene keele:", tolgi_est_rus(word))
+
+    elif choice == "2":
+        word = input("Kirjutage sona vene keeles: ").strip()
+        print("Tolginud sona:", tolgi_rus_est(word))
+
+    elif choice == "3":
+        est = input("Kirjutage uus sona eesti keeles: ").strip()
+        rus = input("Kirjutage uus sona vene keeles: ").strip()
+        print(lisa_sona(est, rus))
+
+    elif choice == "4":
+        old = input("Kirjutage vana sona eesti keeles: ").strip()
+        new_est = input("Kirjutage uus sona eesti keeles: ").strip()
+        new_rus = input("Kirjutage uus sona vene keeles: ").strip()
+
+    elif choice == "5":
+        testi_teadmisi()
+
+    elif choice == "6":
+        print("Head aega!")
+        break
+
+    else:
+        print("Vaale kood, proovi kirjutega uuesti")
